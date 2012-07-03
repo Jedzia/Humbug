add2d2a62361f34b29cb4ce25f9d8d100d082259
Jedzia <jed69@gmx.de>
2012-07-03 18:56:56 +0200

    - More SDL examples - Todo's

--- projects/Playground/SDL/Init01.cpp
+++ projects/Playground/SDL/Init01.cpp
@@ -7,4 +7,19 @@ int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
 	atexit(SDL_Quit);
+	g_pVideoInfo = SDL_GetVideoInfo();
+	fprintf(stdout, "\nVideo Information:\n");
+	fprintf(stdout, "hw_available? %d\n", g_pVideoInfo->hw_available);
+	fprintf(stdout, "wm_available? %d\n", g_pVideoInfo->wm_available);
+	fprintf(stdout, "blit_hw? %d\n", g_pVideoInfo->blit_hw);
+	fprintf(stdout, "blit_hw_CC? %d\n", g_pVideoInfo->blit_hw_CC);
+	fprintf(stdout, "blit_hw_A? %d\n", g_pVideoInfo->blit_hw_A);
+	fprintf(stdout, "blit_sw? %d\n", g_pVideoInfo->blit_sw);
+	fprintf(stdout, "blit_sw_CC? %d\n", g_pVideoInfo->blit_sw_CC);
+	fprintf(stdout, "blit_sw_A? %d\n", g_pVideoInfo->blit_sw_A);
+	fprintf(stdout, "blit_fill? %d\n", g_pVideoInfo->blit_fill);
+	fprintf(stdout, "video memory(in K)? %d\n", g_pVideoInfo->video_mem);
+	fprintf(stdout, "bits per pixel? %d\n", g_pVideoInfo->vfmt->BitsPerPixel);
+
+	return(0);
 }
