8fbdbbae1136426fdef26c2c11ae325c0e57c445
Jedzia <jed69@gmx.de>
2012-09-29 14:34:31 +0200

    indenters update

--- projects/HumbugLib/src/HumbugLib/win32/appinit.cpp
+++ projects/HumbugLib/src/HumbugLib/win32/appinit.cpp
@@ -1,22 +1,35 @@
+/*---------------------------------------------------------*/
+/*!
+ * This file is part of Humbug, the strangest game ever.
+ * License details can be found in the file COPYING.
+ * Copyright (c) 2012, EvePanix. All rights reserved.
+ *
+ * \brief      This file contains the definition of
+ *             the appinit.cpp class.
+ * \folder     $(folder)
+ * \file       appinit.cpp
+ * \date       2012-09-29
+ * \author     Jedzia.
+ *
+ * modified    2012-09-29, Jedzia
+ */
+/*---------------------------------------------------------*/
 #include "../stdafx.h"
 #include <stdio.h>
 #include <windows.h>
 
 //#include <build/cmake/include/debug.h>
 namespace HumbugLIB_NAMESPACE {
-	namespace internal {
+namespace internal {
+std::string getExePath(){
+    char result[ MAX_PATH ];
+    return std::string( result, GetModuleFileNameA( NULL, result, MAX_PATH ) );
+}
 
-		std::string getExePath()
-		{
-			char result[ MAX_PATH ];
-			return std::string( result, GetModuleFileNameA( NULL, result, MAX_PATH ) );
-		}
-
-		std::string buildAppDir(const std::string& exePath)
-		{
-			std::string::size_type lastdirpos = exePath.find_last_of("\\");
-			std::cout << "Dirpos at '" << lastdirpos << "'." << std::endl;
-			return exePath.substr(0, lastdirpos + 1);
-		}
-	}
+std::string buildAppDir(const std::string& exePath){
+    std::string::size_type lastdirpos = exePath.find_last_of("\\");
+    std::cout << "Dirpos at '" << lastdirpos << "'." << std::endl;
+    return exePath.substr(0, lastdirpos + 1);
+}
+}
 }
