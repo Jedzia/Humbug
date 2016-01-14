16d42e4d00868ee4abe890b7cb8e9f74777a0764
Jedzia <jed69@gmx.de>
2016-01-14 18:20:55 +0100

    shape drawing interface, SLine

---
+++ projects/GuiLib/GUI/Components/Shapes/Line.h
@@ -0,0 +1,41 @@
+/*---------------------------------------------------------*/
+/*!
+ * This file is part of Humbug, the strangest game ever.
+ * License details can be found in the file COPYING.
+ * Copyright (c) 2013, EvePanix. All rights reserved.
+ *
+ * \brief      This file contains the definition of
+ *             the Line.h class.
+ * \file       Line.h
+ * \date       2016-01-09
+ * \author     Jedzia.
+ *
+ * modified    2016-01-09, Jedzia
+ */
+/*---------------------------------------------------------*/
+#ifndef __SLine_H__
+#define __SLine_H__
+
+#include "../Canvas.h"
+
+namespace gui {
+namespace components {
+/** @class SLine:
+ *  Aids in drawing lines to several targets. (At the time only CCanvas).
+ */
+class SLine : public CCanvas {
+public:
+
+    /** Render a Line.
+    *  Use this function to draw a line on the specified rendering target.
+    *  @param target The canvas to paint on.
+    *  @param pStart The start point of the line.
+    *  @param pEnd The end point of the line.
+    *  @param color The drawing color of the line or NULL if no color change is wanted.
+    */
+    static void RenderDrawLine(const CCanvas* target, const CPoint& pStart, const CPoint& pEnd, const CColor* color);
+};
+} // namespace components
+} // namespace gui
+
+#endif // ifndef __SLine_H__
