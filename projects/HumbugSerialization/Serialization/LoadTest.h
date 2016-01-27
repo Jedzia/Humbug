d3021f6f29702454b4b9ae02523bf9dc46afdfaf
Jedzia <jed69@gmx.de>
2016-01-27 02:28:48 +0100

    added HumbugSerialization library

---
+++ projects/HumbugSerialization/Serialization/LoadTest.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "../global.h"
+//
+#include <string>
+//
+
+namespace humbug {
+namespace core {
+  /** @class LoadTest:
+   *  Detailed description.
+   */
+  class LoadTest {
+public:
+
+      LoadTest();
+      ~LoadTest();
+
+      /** LoadTest, Str:
+       *  Detailed description.
+       *  @return TODO
+       */
+      std::string Str() const { return str; }
+
+      /** LoadTest, Str:
+       *  Detailed description.
+       *  @param val TODO
+       */
+      void Str(std::string val) { str = val; }
+
+      /** LoadTest, Language:
+       *  Detailed description.
+       *  @param x TODO
+       *  @return TODO
+       */
+      const char * Language(int x) const;
+
+private:
+
+      int i;
+      std::string str;
+  };
+}
+}
