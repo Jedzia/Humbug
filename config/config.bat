c0129c5744902ecc0be9e4f179b2b3306acf68d3
Jedzia <jed69@gmx.de>
2016-02-12 21:28:01 +0100

    works with Boost 1.59

--- config/config.bat
+++ config/config.bat
@@ -6,6 +6,8 @@ SET SDLVER=2.0.4
 if "%BOOST_ROOTDIR%"=="" ( 
 REM SET BOOST_ROOTDIR=E:\Devel\CPP\Libs\boost\boost-1.55.0
 REM SET BOOST_ROOTDIR=E:\Devel\CPP\Libs\boost\boost-git_latest
-SET BOOST_ROOTDIR=E:\Devel\CPP\Libs\boost\boost_1_58_0
+REM SET BOOST_ROOTDIR=E:\Devel\CPP\Libs\boost\boost_1_58_0
+SET BOOST_ROOTDIR=E:\Devel\CPP\Libs\boost\boost_1_59_0
+REM SET BOOST_ROOTDIR=E:\Devel\CPP\Libs\boost\boost_1_60_0
 )
 SET XSD_ROOTDIR=E:\Devel\CPP\Libs\CodeSynthesisXSD-3.3
