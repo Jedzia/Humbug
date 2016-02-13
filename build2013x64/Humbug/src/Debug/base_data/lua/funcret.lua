413c7a944fa7548bbfdb2d01002c55f72771ee02
Jedzia <jed69@gmx.de>
2016-02-13 21:01:24 +0100

    deleted, is created by build

--- build2013x64/Humbug/src/Debug/base_data/lua/funcret.lua
+++
@@ -1,7 +0,0 @@
--- the first program in every language
-
--- io=ioCreate()
--- ioWrite( io, "Hello World from FUNC RET!")
--- ioDelete(io)
-
-io.write("FUNC RET, from ",_VERSION,"!\n")
--- build2013x64/Humbug/src/Debug/base_data/lua/iowrite.lua
+++
@@ -1,5 +0,0 @@
--- write to IO
-io.write("IO Write, from iowrite.lua, ",_VERSION,"!\n")
-io.write("IO Write, Value myVar:(", myVar, ")!\n")
-myVar = "it Changed"
-
