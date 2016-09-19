4a8f15beb6390d31b29a63357b399e3754b727af
Jedzia <jed69@gmx.de>
2016-09-19 20:24:16 +0200

    runtime scripts

--- build2013x64/HumbugGL/src/Debug/base_data/Lua/globalclass.lua
+++ build2013x64/HumbugGL/src/Debug/base_data/Lua/globalclass.lua
@@ -1,5 +1,5 @@
 -- write to IO
-io.write("IO Write, from iowrite.lua, ",_VERSION,"!\n")
+io.write("IO Write, from globalclass.lua, ",_VERSION,"!\n")
 io.write("IO Write, Value myVar:(", myVar, ")!\n")
 
 x=3
